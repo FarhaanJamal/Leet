2668f88198907c2cf750443f6fd1224a3948db70
Farhaan <95125546+FarhaanJamal@users.noreply.github.com>
2025-08-01 13:17:45 +0530

    Time: 39 ms (5.22%) | Memory: 18.3 MB (9.33%) - LeetSync

---
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/minimum-operations-to-make-columns-strictly-increasing.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumOperations(self, grid: List[List[int]]) -> int:
+        ln = len(grid)
+        if ln == 1:
+            return 0
+        ans = 0
+        for i in range(1, ln):
+            for j in range(len(grid[0])):
+                if grid[i][j] > grid[i-1][j]:
+                    continue
+                temp = grid[i-1][j] - grid[i][j] + 1
+                grid[i][j] += temp
+                ans += temp
+                print(i, j, temp, ans)
+        return ans
