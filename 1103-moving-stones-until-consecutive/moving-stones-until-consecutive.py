c075ef0dd4d6add30588b7c2356a0966eb90d9d0
Farhaan <95125546+FarhaanJamal@users.noreply.github.com>
2025-08-13 23:02:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (80.65%) - LeetSync

--- 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.py
+++ 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.py
@@ -6,7 +6,7 @@ class Solution(object):
         :type c: int
         :rtype: List[int]
         """
-        x, y, z = sorted([a, b, c])
+        """x, y, z = sorted([a, b, c])
         ans = [0, 0]
         diff1 = abs(y-x) -1
         if diff1 > 0:
@@ -19,5 +19,15 @@ class Solution(object):
             if diff2 == 1:
                 ans[0] = 1
             ans[1] += diff2
-        return ans
+        return ans"""
+        x, y, z = sorted([a, b, c])
+        min_move, max_move = 0, 0
+        if y-x == 1 and z-y == 1:
+            min_move = 0
+        elif y-x > 2 and z-y > 2:
+            min_move = 2
+        else:
+            min_move = 1
+        max_move = (y-x) + (z-y) - 2 
+        return [min_move, max_move]
         
