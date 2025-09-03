617a7f80c3932fea0c6e1cff7c186e49f188785e
Farhaan <95125546+FarhaanJamal@users.noreply.github.com>
2025-09-03 19:45:49 +0530

    Time: 5 ms (0.94%) | Memory: 17.9 MB (15.33%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,25 +1,31 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def two_pointers(s):
-            ln = len(s)
-            if ln == 0:
-                return []
-            l, r = -1, 0
-            lst = list(s)
-            while l < r and r < len(lst):
-                print(l, r, lst)
-                if lst[r] == "#":
-                    if l != -1:
-                        lst.pop(l)
-                        l-=1
-                        r-=1
-                    lst.pop(r)
-                    if r > 0:
-                        l-=1
-                        r-=1                    
-                    continue
-                r+=1
-                l+=1
-            return lst
-        print(two_pointers(s), two_pointers(t))
-        return two_pointers(s) == two_pointers(t)
+        def perform_backspace(string, pointer):
+            backspace = 0
+            while pointer >= 0:
+                print(pointer)
+                if string[pointer] == "#":
+                    backspace += 1
+                elif backspace > 0:
+                    backspace -=1
+                else:
+                    break
+                pointer -=1
+            return pointer
+
+        s_p, t_p = len(s)-1, len(t)-1
+        while s_p >= 0 or t_p >= 0:
+            print(s_p, t_p)
+            s_p = perform_backspace(s, s_p)
+            t_p = perform_backspace(t, t_p)
+            if s_p < 0 and t_p < 0:
+                return True
+            if s_p < 0 or t_p < 0:
+                return False
+            if s[s_p] != t[t_p]:
+                return False
+            s_p -= 1
+            t_p -= 1
+        
+        return True
+
